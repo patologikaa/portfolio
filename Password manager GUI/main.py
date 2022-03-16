83cb5f2bbe2b211eb39a4336a279d24e0abcb072
patologikaa <44196177+patologikaa@users.noreply.github.com>
2022-03-16 11:25:08 +0100

    Password Manager upgrade
    JSON updated

--- Password manager GUI/main.py
+++ Password manager GUI/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
@@ -44,41 +46,73 @@ def add():
     mail = mail_entry.get()
     passwd = pass_entry.get()
 
+    new_data = {
+        web: {
+            "mail": mail,
+            "password": passwd
+        }
+    }
     if len(web) == 0 or len(passwd) == 0:
         messagebox.showinfo(title="Error", message="Please fill all inputs")
     else:
-        is_ok = messagebox.askokcancel(title=web,
-                                       message=f"These are the details entered: \n Email: {mail} \n Password: {passwd} \n Is it okay to save?")
-        if is_ok:
-            with open("passwd.txt", 'a') as f:
-                f.write(web + " | " + mail + " | " + passwd + '\n')
+        try:
+            with open('data.json', 'r') as file:
+                data = json.load(file)
+
+        except FileNotFoundError:
+            with open('data.json', 'w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open('data.json', 'w') as file:
+                json.dump(data, file, indent=4)
+        finally:
             web_entry.delete(0, END)
             pass_entry.delete(0, END)
+#-----------------------FIND PASSWORD --------------------------- #
+def searchService():
+    try:
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message=f"There's no database file")
 
+    else:
+        service = web_entry.get()
+        if service in data:
+            data_pass = data[service]["password"]
+            data_mail = data[service]["mail"]
+            messagebox.showinfo(title="Password", message=f"Your mail is: {data_mail} \n Your password is: {data_pass}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No such service")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 web = Label(text="Website ")
-mail = Label(text="Email/Username ")
+mail = Label(text="Mail/Login")
 password = Label(text="Password ")
-generate = Button(text="Generate", command=generate)
-add = Button(text="Add", width=35, command=add)
+generate = Button(text="Generate", width=10, command=generate)
+add = Button(text="Add", width=25, command=add)
+search = Button(text="Search", width=10, command=searchService)
+web_entry = Entry(width=30)
 
-web_entry = Entry(width=35)
-
-web_entry = Entry(width=45)
+web_entry = Entry(width=32)
 mail_entry = Entry(width=45)
-pass_entry = Entry(width=35)
+pass_entry = Entry(width=32)
 
 web.grid(column=0, row=1)
-web_entry.grid(column=1, row=1, columnspan=2)
+web_entry.grid(column=1, row=1)
 web_entry.focus()
+
 mail.grid(column=0, row=2)
 mail_entry.grid(column=1, row=2, columnspan=2)
 mail_entry.insert(0, "yourEmal@gmail.com")
+
 password.grid(column=0, row=3)
 pass_entry.grid(column=1, row=3)
-add.grid(column=1, row=4, rowspan=2, columnspan=2)
+
+add.grid(column=1, row=4)
 generate.grid(column=2, row=3)
+search.grid(column=2,row=1)
 
 window.mainloop()
